3ea9e940b72767e6d67fff93565ef553dedf6318
nowknowing <s.sunaga00@gmail.com>
2020-06-10 21:16:11 +0800

    Only OpenCV loa

--- app/src/main/java/com/gamebois/amaaze/MainActivity.java
+++ app/src/main/java/com/gamebois/amaaze/MainActivity.java
@@ -3,8 +3,21 @@ package com.gamebois.amaaze;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+
+    static{
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV Working");
+        } else {
+            Log.d(TAG, "OpenCV Not Working or Unsuccessful");
+        }
+    }
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
