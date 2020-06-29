16eab4bbd59eee4c1462c389b79210081a4c3a1c
nowknowing <s.sunaga00@gmail.com>
2020-06-29 13:15:04 +0800

    Second Try only Camera Activity
    (cherry picked from commit 7b960e796f33ce79613e416f89afd9aa45a8061e)

--- app/src/main/java/com/gamebois/amaaze/CameraCaptureActivity.java
+++ app/src/main/java/com/gamebois/amaaze/CameraCaptureActivity.java
@@ -17,25 +17,38 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CameraCaptureActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    int mat_width = 1280;
+    int mat_height = 960;
+
+
     private static String TAG = "OpenCVCamera";
     private int CAMERA_REQ = 10;
-    CameraBridgeViewBase cameraBridgeViewBase;
+    CameraBridgeViewBase cameraBridgeviewbase;
     Mat frame;
+    Mat filtered;
+    Mat processed;
+    List<MatOfPoint> contours = new ArrayList<>();
     DetectMaze detectMaze;
-
     BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
             super.onManagerConnected(status);
-            if (status == BaseLoaderCallback.SUCCESS) {
-                cameraBridgeViewBase.enableView();
+            if (status == BaseLoaderCallback.SUCCESS){
+                cameraBridgeviewbase.enableView();
             } else {
                 super.onManagerConnected(status);
             }
@@ -47,23 +60,43 @@ public class CameraCaptureActivity extends AppCompatActivity implements CameraBr
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_camera_capture);
-        cameraBridgeViewBase = (JavaCameraView) findViewById(R.id.my_camera_view);
-        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
-        cameraBridgeViewBase.setCvCameraViewListener(this);
+        cameraBridgeviewbase = (JavaCameraView) findViewById(R.id.my_camera_view);
+        cameraBridgeviewbase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeviewbase.setCvCameraViewListener(this);
 
     }
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        contours.clear();
         frame = inputFrame.rgba();
-        detectMaze.process();
+        Mat hierarchy = new Mat();
+        Imgproc.cvtColor(frame, processed, Imgproc.COLOR_RGBA2GRAY);
+        Imgproc.bilateralFilter(processed, filtered, 3, 170, 3);
+        Imgproc.adaptiveThreshold(filtered, processed, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 7, 7); // apply adaptive thresolding to blured frame
+        Imgproc.dilate(processed, processed, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(17, 17), new Point(0, 0)));
+        Imgproc.erode(processed, processed, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(17, 17), new Point(0, 0)));
+        Imgproc.findContours(processed, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+        double maxVal = 0;
+        int hello = 0;
+        for (int contourIdx = 0; contourIdx < contours.size(); contourIdx++) {
+            double contourArea = Imgproc.contourArea(contours.get(contourIdx));
+            if (maxVal < contourArea) {
+                maxVal = contourArea;
+                hello = contourIdx;
+            }
+        }
+        if( hello >=0) {
+            Imgproc.drawContours(frame, contours, hello, new Scalar(0, 255, 0), 2, Core.LINE_8, hierarchy, 0, new Point());
+        }
         return frame;
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        frame = new Mat(height, width, CvType.CV_8UC4);
-        detectMaze = new DetectMaze(cameraBridgeViewBase.getWidth(), cameraBridgeViewBase.getHeight(), cameraBridgeViewBase.resolu);
+        frame = new Mat();
+        processed = new Mat();
+        filtered = new Mat();
 
     }
 
@@ -71,8 +104,8 @@ public class CameraCaptureActivity extends AppCompatActivity implements CameraBr
     @Override
     public void onCameraViewStopped() {
         frame.release();
-        //filtered.release();
-        //filtered.release();
+        processed.release();
+        filtered.release();
     }
 
     @Override
@@ -90,8 +123,8 @@ public class CameraCaptureActivity extends AppCompatActivity implements CameraBr
     @Override
     protected void onPause() {
         super.onPause();
-        if (cameraBridgeViewBase != null) {
-            cameraBridgeViewBase.disableView();
+        if (cameraBridgeviewbase != null) {
+            cameraBridgeviewbase.disableView();
         }
 
     }
@@ -100,8 +133,8 @@ public class CameraCaptureActivity extends AppCompatActivity implements CameraBr
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (cameraBridgeViewBase != null) {
-            cameraBridgeViewBase.disableView();
+        if (cameraBridgeviewbase != null) {
+            cameraBridgeviewbase.disableView();
         }
     }
 
--- app/src/main/java/com/gamebois/amaaze/DetectMaze.java
+++ app/src/main/java/com/gamebois/amaaze/DetectMaze.java
@@ -1,8 +1,78 @@
 package com.gamebois.amaaze;
 
+import android.graphics.PointF;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class DetectMaze {
 
-    public DetectMaze()
-    public void process() {
+    private Mat processed;
+    private Mat filtered;
+    private Mat hierarchy;
+    private Size unhole;
+    private Scalar color;
+    private List<MatOfPoint> contours;
+    private double maxContourArea;
+    private int maxContourIdx;
+    float scale;
+    float xoffset;
+    float yoffset;
+
+    public DetectMaze(int screen_width,int screen_height, int mat_width, int mat_height) {
+        processed = new Mat();
+        filtered = new Mat();
+        hierarchy = new Mat();
+        unhole = new Size(15,15);
+        color = new Scalar(0, 255, 0);
+        List<MatOfPoint> contours = new ArrayList<>();
+        maxContourArea = 0;
+        maxContourIdx = 0;
+        scale= Math.min( (float)screen_width/mat_width, (float)screen_height/mat_height );
+        xoffset= (float) ((screen_width-scale*mat_width)/2.0); //shift the points X coordiante by xoffset (defined in opencv CameraBridgeViewBase.java at line 420)
+        yoffset=(float) ((screen_height-scale*mat_height)/2.0);
+    }
+
+    public List<PointF> getMazePoints() {
+        List<Point> cv = contours.get(maxContourIdx).toList();
+        List<PointF> android = new ArrayList<>();
+
+        for (int j = 0; j < cv.size(); j++) {
+            Point p = cv.get(j);
+            android.add(new PointF((float) p.x*scale + xoffset, (float) (p.y*scale+yoffset)));
+        }
+
+        return  android;
+
+    }
+    
+
+    public void process(Mat frame) {
+        Imgproc.cvtColor(frame, processed, Imgproc.COLOR_RGBA2GRAY);
+        Imgproc.bilateralFilter(processed, filtered, 3, 170, 3);
+        Imgproc.adaptiveThreshold(filtered, processed, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 7, 7); // apply adaptive thresolding to blured frame
+        Imgproc.dilate(processed, processed, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, unhole, new Point(-1, -1)));
+        Imgproc.erode(processed, processed, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, unhole, new Point(-1, -1)));
+
+        Imgproc.findContours(processed, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+        for (int i = 0; i< contours.size(); i++) {
+            double contourArea = Imgproc.contourArea(contours.get(i));
+            if (maxContourArea < contourArea) {
+                maxContourArea = contourArea;
+                maxContourIdx = i;
+            }
+        }
+
+        Imgproc.drawContours(frame, contours, maxContourIdx, color, 2, Core.LINE_8, hierarchy, 0, new Point());
     }
 }
